28deb0cb7da13193cf6c28b843ec2dbad87f5153
FooBarFighters <michael.tjaden@gmail.com>
2021-06-14 23:09:16 +0200

    Add Api exceptions

---
+++ src/ZendServer/WebApi/Exception/InternalServerError.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace FooBarFighters\ZendServer\WebApi\Exception;
+
+/**
+ * Operation against remote server failed.
+ *
+ * @code 500
+ *
+ * Class InternalServerError
+ * @package FooBarFighters\ZendServer\WebApi\Exception
+ */
+class InternalServerError extends ApiException
+{
+
+}
---
+++ src/ZendServer/WebApi/Exception/LogFileNotReadableException.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace FooBarFighters\ZendServer\WebApi\Exception;
+
+/**
+ * Log file was not found / missing read permissions.
+ *
+ * @code: 404
+ *
+ * Class LogFileNotReadableException
+ * @package FooBarFighters\ZendServer\WebApi\Exception
+ */
+class LogFileNotReadableException extends ApiException
+{
+
+}
